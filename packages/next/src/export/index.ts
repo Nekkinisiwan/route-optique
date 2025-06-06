8e116398d5821582922fe3f76da524e67729db25
Stefan Schubert <stefan.schubert@sbb.ch>
2025-06-06 09:55:47 +0200

    feat: add suffix to rsc txt files for easy routing
    * enables easy handling for cdns or proxies to identify rsc payloads and route them correctly beside other txt files

--- packages/next/src/export/index.ts
+++ packages/next/src/export/index.ts
@@ -874,7 +874,7 @@ async function exportAppImpl(
               outDir,
               `${route}${
                 subFolders && route !== '/index' ? `${sep}index` : ''
-              }.txt`
+              }.rsc.txt`
             )
           : join(pagesDataDir, `${route}.json`)
 
