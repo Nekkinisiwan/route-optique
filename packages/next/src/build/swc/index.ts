afd9a50141994c2d1343856eae04223772454723
JamBalaya56562 <jambalaya.pyoncafe@outlook.jp>
2025-08-14 15:19:22 +0900

    chore: fix typos in next package

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -1151,7 +1151,7 @@ async function loadWasm(importPath = '') {
     return newObj
   }
 
-  // Note wasm binary does not support async intefaces yet, all async
+  // Note wasm binary does not support async interfaces yet, all async
   // interface coereces to sync interfaces.
   wasmBindings = {
     css: {
